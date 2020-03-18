83cc22588f4d068437b8b422406ed9e4014d4bb9
Vinicius da Silva <vssilva233@gmail.com>
2020-03-18 11:52:08 -0400

    [NEW] Create interface for Video

---
+++ src/interfaces/videos.interface.ts
@@ -0,0 +1,10 @@
+export interface Video {
+  id: number,
+  name: string,
+  duration: number,
+  provider: string,
+  media_type: string,
+  provider_id: string,
+  expires_at: number,
+  watched?: boolean,
+}
