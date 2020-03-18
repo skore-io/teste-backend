bf8f9ebabd8e37825a4fd3375ff848cc11af60a7
Vinicius da Silva <vssilva233@gmail.com>
2020-03-18 11:49:54 -0400

    [NEW] Creating DTO for updating and creating video

---
+++ src/dto/update-video.dto.ts
@@ -0,0 +1,10 @@
+export class UpdateVideoDto {
+  readonly name: string;
+  readonly duration: number;
+  readonly provider: string;
+  readonly media_type: string;
+  readonly provider_id: string;
+  readonly expires_at: number;
+  readonly id: number;
+}
+
