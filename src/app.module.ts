ec651789ef68d9f8f98b2c52e303cc5cc5854d2d
Vinicius da Silva <vssilva233@gmail.com>
2020-03-18 11:51:30 -0400

    initial project

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { VideosController } from './videos/videos.controller';
+import { VideosService } from './services/videos.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController, VideosController],
+  providers: [VideosService],
+})
+export class AppModule {}
