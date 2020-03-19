ee96b27ea74829948e3e5925b27610744ca03beb
Vinicius da Silva <vssilva233@gmail.com>
2020-03-19 02:34:44 -0400

    [UPDATE] rename file videoListing to video-listing

--- src/videos/videos.controller.ts
+++ src/videos/videos.controller.ts
@@ -3,7 +3,7 @@ import { VideosService } from '../services/videos.service';
 import { CreateVideoDto } from '../dto/create-video.dto';
 import { UpdateVideoDto } from '../dto/update-video.dto';
 import { VideoEntity } from '../serializers/video.entity';
-import { VideoListingEntity } from '../serializers/videoListing.entity';
+import { VideoListingEntity } from '../serializers/video-listing.entity';
 
 @Controller('videos')
 export class VideosController {
