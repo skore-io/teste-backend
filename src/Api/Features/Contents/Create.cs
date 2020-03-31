5b26e42850e156a5a8666e019e1dbaa337815e8b
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-31 13:19:11 -0300

    Adicionar command para Criar conteúdo

---
+++ src/Api/Features/Contents/Create.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Api.Models;
+
+using MediatR;
+
+namespace Api.Features.Contents
+{
+    public class Create : IRequest<Conteudo>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Duration { get; set; }
+
+        public string Provider { get; set; }
+
+        public string MediaType { get; set; }
+
+        public string ProviderId { get; set; }
+
+        public long ExpiresAt { get; set; }
+    }
+
+    public class CreateHandler : IRequestHandler<Create, Conteudo>
+    {
+        private readonly ICollection<Conteudo> _conteudos;
+
+        public CreateHandler(ICollection<Conteudo> conteudos)
+        {
+            _conteudos = conteudos ?? throw new ArgumentNullException(nameof(conteudos));
+        }
+
+        public Task<Conteudo> Handle(Create request, CancellationToken cancellationToken)
+        {
+            if (request is null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            if (_conteudos.Any(c => c.Id == request.Id))
+            {
+                throw new InvalidOperationException($"Um conteúdo com o id {request.Id} já existe");
+            }
+
+            var novoConteudo = new Conteudo
+            {
+                Id = request.Id,
+                Name = request.Name,
+                Duration = request.Duration,
+                Provider = request.Provider,
+                ProviderId = request.ProviderId,
+                ExpiresAt = request.ExpiresAt
+            };
+
+            _conteudos.Add(novoConteudo);
+
+            return Task.FromResult(novoConteudo);
+        }
+    }
+}
