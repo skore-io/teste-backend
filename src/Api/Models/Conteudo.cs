5ee90c5ab516b0f260f47aa1d9802ab19976c1f3
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-31 12:32:43 -0300

    Adicionar model para o Conteudo

---
+++ src/Api/Models/Conteudo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Api.Models
+{
+    public class Conteudo
+    {
+        private DateTimeOffset _expiresAt;
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Duration { get; set; }
+
+        public string Provider { get; set; }
+
+        public string MediaType { get; set; }
+
+        public long ExpiresAt
+        {
+            get => _expiresAt.ToUnixTimeSeconds();
+            set => _expiresAt = DateTimeOffset.FromUnixTimeSeconds(value);
+        }
+
+        public bool Watched { get; set; } = false;
+
+        public bool Expired => ExpiresAt < DateTimeOffset.Now.ToUnixTimeSeconds();
+    }
+}
