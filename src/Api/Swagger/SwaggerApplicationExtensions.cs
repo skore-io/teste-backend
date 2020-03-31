0760180df0cfb13856437f7e818d12508f9d6092
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-31 12:54:40 -0300

    Configurar geração e exibição do Swagger Docs

---
+++ src/Api/Swagger/SwaggerApplicationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class SwaggerApplicationExtensions
+    {
+        public static IApplicationBuilder UseSwaggerGenAndUi(this IApplicationBuilder app)
+        {
+            if (app is null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            app
+                .UseSwagger()
+                .UseSwaggerUI(c =>
+                {
+                    c.RoutePrefix = string.Empty;
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Teste Backend API");
+                });
+
+            return app;
+        }
+    }
+}
