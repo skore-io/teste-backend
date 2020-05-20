33f559d0b3e2bff34af91033329807beedf0d714
Djair Carvalho <djaircarvalho.dj7@gmail.com>
2020-05-20 14:02:04 -0300

    some test refactoring

--- test/contents/controllers/contents.controller.spec.ts
+++ test/contents/controllers/contents.controller.spec.ts
@@ -6,39 +6,24 @@ import { BadRequestException } from '@nestjs/common';
 
 
 
+
 describe('Contents Controller', () => {
   let controller: ContentsController;
   let createContent: CreateContent;
-
-  const validContent = new ContentInputData(
-    1,
-    "GOTO 2017 • The Many Meanings of Event-Driven Architecture • Martin Fowler",
-     3006,
-    "youtube",
-    "video",
-    "STKCRSUsyP0",
-    1580428851394,
-  )
-  const expectedReturn = new Content(
-    1,
-    "GOTO 2017 • The Many Meanings of Event-Driven Architecture • Martin Fowler",
-     3006,
-    "youtube",
-    "video",
-    "STKCRSUsyP0",
-    1580428851394,
-  )
-
+  const validContent = new ContentInputData(1, "GOTO 2017 • The Many Meanings of Event-Driven Architecture • Martin Fowler", 3006, "youtube", "video", "STKCRSUsyP0", 1580428851394);
+  
+  
   beforeEach(async () => {
     createContent = new CreateContent()
     controller = new ContentsController(createContent)
   });
-
+  
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
-
+  
   describe('create content', ()=> {
+    const expectedReturn = new Content(1, "GOTO 2017 • The Many Meanings of Event-Driven Architecture • Martin Fowler", 3006, "youtube", "video", "STKCRSUsyP0", 1580428851394);
     describe('success', () => {
       it('returns the content', () =>{
         const createResult = {content: expectedReturn, isSuccess: true, errors: [] }
