574c5d1cd9a25bcfec4f58bb7f5603617343b739
christopher <christopher.fernandes@before.com.br>
2020-09-07 00:41:43 -0400

    criado configuracao do banco

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,11 +1,14 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+import { VideoSubscriber } from '../video/subscribers/video.subscriber'
+import { VideoModel } from '../video/models/video.model'
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: process.env.TYPEORM_HOST,
-  port: parseInt(process.env.TYPEORM_PORT),
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
-  entities: [__dirname + '/../**/*.model.ts'],
+  host: 'db',
+  port: 3306,
+  username: 'root',
+  password: 'root',
+  database: 'nest',
+  entities: [VideoModel],
+  subscribers: [VideoSubscriber],
 }
